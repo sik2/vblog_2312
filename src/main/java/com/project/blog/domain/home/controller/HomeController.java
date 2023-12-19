89393a2f9e03f68c10d3afca66c6990c94ac3ce4
sik2 <bickwang123@gmail.com>
2023-12-19 11:22:14 +0900

    10강 1부, 로그인 기능 구현을 위한 셋팅

--- src/main/java/com/project/blog/domain/home/controller/HomeController.java
+++ src/main/java/com/project/blog/domain/home/controller/HomeController.java
@@ -2,13 +2,11 @@ package com.project.blog.domain.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
     public String home () {
-        return "hello";
+        return "home/main";
     }
 }
--- src/main/java/com/project/blog/domain/member/repository/MemberRepository.java
+++ src/main/java/com/project/blog/domain/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.project.blog.domain.member.repository;
 import com.project.blog.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByusername(String username);
 }
