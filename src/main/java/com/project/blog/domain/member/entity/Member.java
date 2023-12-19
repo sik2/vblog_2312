e5e30cdce911c045edde4d98160a200f29012f24
sik2 <bickwang123@gmail.com>
2023-12-19 10:26:53 +0900

    7강, Member 엔티티 등록, 엔티티 수정

---
+++ src/main/java/com/project/blog/domain/member/entity/Member.java
@@ -0,0 +1,27 @@
+package com.project.blog.domain.member.entity;
+
+import com.project.blog.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Member extends BaseEntity {
+    @Comment("유저아이디")
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @Column(unique = true)
+    private String nickname;
+    private String email;
+}
