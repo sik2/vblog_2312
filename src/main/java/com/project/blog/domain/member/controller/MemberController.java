f7f525cd2c3d66387cc3bbb6dbf9a15cd5cae671
sik2 <bickwang123@gmail.com>
2023-12-21 10:39:58 +0900

    일반 회원가입 구현 nested class 를 통한 form 벨리데이션 처리

--- src/main/java/com/project/blog/domain/member/controller/MemberController.java
+++ src/main/java/com/project/blog/domain/member/controller/MemberController.java
@@ -1,13 +1,25 @@
 package com.project.blog.domain.member.controller;
 
+import com.project.blog.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    private final MemberService memberService;
 
     @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
@@ -18,6 +30,36 @@ public class MemberController {
     @GetMapping("/join")
     public String joinPage() {
         return "member/join";
+    }
+
+    @PostMapping("join")
+    public String join (@Valid JoinForm joinForm) {
+
+        memberService.join(joinForm.getUsername(), joinForm.getNickname(), joinForm.getPassword(), joinForm.email);
+
+        return "redirect:/member/login";
+    }
+
+
+    @Getter
+    @ToString
+    @AllArgsConstructor
+    public static class JoinForm {
+        @NotBlank
+        @Length(min = 4)
+        private String username;
+        @NotBlank
+        @Length(min = 4)
+        private String nickname;
+        @NotBlank
+        @Length(min = 4)
+        private String password;
+        @NotBlank
+        @Length(min = 4)
+        private String password_confirm;
+        @NotBlank
+        @Length(min = 4 )
+        private String email;
     }
 
 }
